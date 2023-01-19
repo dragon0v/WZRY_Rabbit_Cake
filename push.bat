bf19501ab37a54d2d9fa26b4c9884cd5ce45146b
dragon0v <2307022423@qq.com>
2023-01-20 01:50:31 +0800

    update readme

--- push.bat
+++ push.bat
@@ -3,6 +3,11 @@
 @REM git remote add origin git@github.com:dragon0v/WZRY_Rabbit_Cake.git
 @REM git commit -m "first commit"
 @REM git branch -M main
+
+@REM git config --global i18n.commitencoding utf-8
+@REM git config --global i18n.logoutputencoding utf-8
+@REM git config --global gui.encoding utf-8
+
 git add .
-git commit -m "update main 使用parser传参"
+git commit -m "update readme"
 git push -u origin main
